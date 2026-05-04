bba65ec8e7fdf468aca8071b09973c80d1750354
Serhii <serhii.vovkotrub@gmail.com>
2026-05-04 10:52:11 +0300

    fixed menu screen

--- lib/features/menu/presentation/cubit/menu_state.dart
+++ lib/features/menu/presentation/cubit/menu_state.dart
@@ -3,47 +3,45 @@ import '../../../../core/data/models/flow_preferences_model.dart';
 import '../../domain/entities/menu_item.dart';
 import '../../domain/enums/menu_enums.dart';
 
-enum MenuMode { fullMenu, recommended }
-
 class MenuState extends Equatable {
   final List<MenuItem> allItems;
   final List<MenuItem> visibleItems;
   final MenuCategory selectedCategory;
-  final MenuMode mode;
   final bool isLoading;
   final String? error;
   final FlowPreferencesModel currentConfig;
+  final List<MenuCategory> availableCategories;
 
   const MenuState({
     this.allItems = const [],
     this.visibleItems = const [],
     this.selectedCategory = MenuCategory.all,
-    this.mode = MenuMode.fullMenu,
     this.isLoading = false,
     this.error,
     this.currentConfig = const FlowPreferencesModel(),
+    this.availableCategories = const [MenuCategory.all],
   });
 
   MenuState copyWith({
     List<MenuItem>? allItems,
     List<MenuItem>? visibleItems,
     MenuCategory? selectedCategory,
-    MenuMode? mode,
     bool? isLoading,
     String? error,
     FlowPreferencesModel? currentConfig,
     FlowType? currentFlowType,
     Set<DietaryPreference>? dietaryPreferences,
     Set<MajorAllergen>? majorAllergens,
+    List<MenuCategory>? availableCategories,
   }) {
     return MenuState(
       allItems: allItems ?? this.allItems,
       visibleItems: visibleItems ?? this.visibleItems,
       selectedCategory: selectedCategory ?? this.selectedCategory,
-      mode: mode ?? this.mode,
       isLoading: isLoading ?? this.isLoading,
       error: error,
       currentConfig: currentConfig ?? this.currentConfig,
+      availableCategories: availableCategories ?? this.availableCategories,
     );
   }
 
@@ -52,7 +50,6 @@ class MenuState extends Equatable {
     allItems,
     visibleItems,
     selectedCategory,
-    mode,
     isLoading,
     error,
     currentConfig,
